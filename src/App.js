2e147f06a050dca225f451988e063ccbc01e05fb
James Scavuzzo <scavuzzo.11@buckeyemail.osu.edu>
2025-10-12 20:13:13 -0400

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Purchase from './purchase';
 import PaymentEntry from './paymentEntry';
@@ -11,28 +10,14 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Routes>
-            <Route path='/purchase' element={<Purchase/>} />
-            <Route path="/" element={<Navigate replace to="/purchase" />} />
-            <Route path='/purchase/paymentEntry' element={<PaymentEntry/>} />
-            <Route path='/purchase/shippingEntry' element={<ShippingEntry/>} />
-            <Route path='/purchase/viewOrder' element={<ViewOrder/>} />
-            <Route path='/purchase/viewConfirmation' element={<Confirmation/>} />
-          </Routes>
-        </header>
+        <Routes>
+          <Route path='/purchase' element={<Purchase/>} />
+          <Route path="/" element={<Navigate replace to="/purchase" />} />
+          <Route path='/purchase/paymentEntry' element={<PaymentEntry/>} />
+          <Route path='/purchase/shippingEntry' element={<ShippingEntry/>} />
+          <Route path='/purchase/viewOrder' element={<ViewOrder/>} />
+          <Route path='/purchase/viewConfirmation' element={<Confirmation/>} />
+        </Routes>
       </div>
     </Router>
   );
